84b748b5c2bc4f4cc0c317d5afdd2417882fd93f
kurenkov004 <mary.kurenkov@gmail.com>
2024-10-01 18:55:41 -0400

    production-ready updates

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -19,11 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dj&6*_j#-zu55q&d%ir*$2r!0_m2yp-&*7dzw!pk*rd!-v9-i4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
